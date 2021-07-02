b935f9ea2ff2497ee1b280ac9d85270e58162832
roysudeepto168@gmil.com <roysudeepto168@gmil.com>
2021-07-02 13:36:52 +0530

    initialized

---
+++ src/main/java/com/library/sroy/LibraryProject/LibraryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.library.sroy.LibraryProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/library/sroy/LibraryProject/LibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.sroy.LibraryProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
